80a6c26939d25e009a1f6473be4d263d6c133b0d
Gene Howell <gene.howell9@gmail.com>
2021-02-04 15:50:35 -0600

    incorperated flexbox and imported icons (trash/delete icon) for app

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyb
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import Sandbox from './components/sandbox';
 
 export default function App() {
 
@@ -34,6 +35,7 @@ export default function App() {
   }
 
   return (
+    // <Sandbox />
     <TouchableWithoutFeedback onPress={() =>{
       Keyboard.dismiss();
       console.log('dismissed keyboard')
@@ -63,8 +65,10 @@ const styles = StyleSheet.create({
   },
   content:{
     padding: 40,
+    flex: 1,
   },
   list:{
+    flex: 1,
     marginTop: 20,
   }
  
